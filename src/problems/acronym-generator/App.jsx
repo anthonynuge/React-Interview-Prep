9dadbe810bc36fa0fcd5ac05a1ddfaead0dadbe3
antho <anthonynuge0509@gmail.com>
2026-05-27 20:45:40 -0500

    Add acronymGenerator problem.

---
+++ src/problems/acronym-generator/App.jsx
@@ -0,0 +1,10 @@
+import AcronymGenerator from "./AcronymGenerator";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <AcronymGenerator />
+    </div>
+  );
+}
