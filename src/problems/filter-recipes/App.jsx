5b685684415dab547b72404181e405c391caffe3
antho <anthonynuge0509@gmail.com>
2026-05-27 19:39:17 -0500

    Seed first problem.

---
+++ src/problems/filter-recipes/App.jsx
@@ -0,0 +1,6 @@
+import "./styles.css";
+import RecipeFilterApp from "./RecipeFilterApp.jsx";
+
+export default function App() {
+  return <RecipeFilterApp />;
+}
---
+++ src/problems/filter-recipes/RecipeFilterApp.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const RecipeFilterApp = () => {
+  return (
+    <div>
+      <h1>🍽️ Recipe Explorer</h1>
+    </div>
+  );
+};
+
+export default RecipeFilterApp;
